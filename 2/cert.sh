ab3d27a36b005828c365b0383c62a5e19743da7b
vins <82468311+vinstechmy@users.noreply.github.com>
2021-10-12 14:52:13 +0800

    Update cert.sh

--- 2/cert.sh
+++ 2/cert.sh
@@ -13,16 +13,18 @@ systemctl stop v2ray
 systemctl stop v2ray@none
 systemctl stop v2ray@vless
 systemctl stop v2ray@vnone
-systemctl stop xray-mini
 systemctl stop trojan
+systemctl stop xray-mini@vless-direct
+systemctl stop xray-mini@vless-splice
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
 systemctl start v2ray
 systemctl start v2ray@none
 systemctl start v2ray@vless
 systemctl start v2ray@vnone
-systemctl start xray-mini
 systemctl start trojan
+systemctl start xray-mini@vless-direct
+systemctl start xray-mini@vless-splice
 /root/.acme.sh/acme.sh --i
 echo Done
 sleep 0.5
