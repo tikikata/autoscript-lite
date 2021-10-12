d8d9e1e442e548deb91c0bea8669fe7a31dbd679
vins <82468311+vinstechmy@users.noreply.github.com>
2021-10-12 15:07:26 +0800

    Update cert.sh

--- 2/cert.sh
+++ 2/cert.sh
@@ -9,12 +9,28 @@ echo start
 sleep 0.5
 source /var/lib/premium-script/ipvps.conf
 domain=$IP
+systemctl stop xray-mini@vless-direct
+systemctl stop xray-mini@vless-splice
+systemctl disable xray-mini@vless-direct
+systemctl disable xray-mini@vless-splice
 systemctl stop v2ray
 systemctl stop v2ray@none
+systemctl stop v2ray@vless
+systemctl stop v2ray@vnone
+systemctl stop trojan
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
+systemctl enable xray-mini@vless-direct
+systemctl enable xray-mini@vless-splice
+systemctl start xray-mini@vless-direct
+systemctl start xray-mini@vless-splice
+systemctl restart xray-mini@vless-direct
+systemctl restart xray-mini@vless-splice
 systemctl start v2ray
 systemctl start v2ray@none
+systemctl stop v2ray@vless
+systemctl stop v2ray@vnone
+systemctl start trojan
 /root/.acme.sh/acme.sh --i
 echo Done
 sleep 0.5
