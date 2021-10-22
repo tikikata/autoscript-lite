1b3eaa721b89fa67ecddd12e8d30ec34584ab7b0
vinstechmy <82468311+vinstechmy@users.noreply.github.com>
2021-10-22 12:54:38 +0800

    Add files via upload

---
+++ 6/about.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+clear
+echo -e "==================================================="
+echo -e "#             Autoscript By Vinstechmy             #"
+echo -e "#--------------------------------------------------#"
+echo -e "#  For Debian 9 & Debian 10 64 bit                 #"
+echo -e "#  For Ubuntu 18.04 & Ubuntu 20.04 64 bit          #"
+echo -e "#  For VPS with KVM and VMWare virtualization      #"
+echo -e "#  Autoscript By Vinstechmy                        #"
+echo -e "#--------------------------------------------------#"
+echo -e "#         THANKS FOR PURCHASE THIS SCRIPTS         #"
+echo -e "#--------------------------------------------------#"
+echo -e "#                                                  #"
+echo -e "#  Thanks to : Horasss & Wildy-Project             #"
+echo -e "#                                                  #"
+echo -e "#  Telegram : t.me/vinstechmy                      #"
+echo -e "#                                                  #"
+echo -e "#--------------------------------------------------#"
+echo -e "#          © Copyright 2021 by Vinstechmy          #"
+echo -e "===================================================="
---
+++ 6/info.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+clear
+neofetch
+cat /root/log-install.txt
+echo -e ""
+echo -e "Autoscript By Vinstechmy"
+echo ""
